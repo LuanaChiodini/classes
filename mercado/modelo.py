d12a219f11462888d37efbe524ca0e4230a30631
LuanaChiodini <lchiodini87@gmail.com>
2019-05-24 11:13:47 -0300

    mercado

---
+++ mercado/modelo.py
@@ -0,0 +1,31 @@
+class Produto():
+	def __init__(self, codigo, preco):
+		self.codigo = codigo 
+		self.preco = preco
+
+class Item():
+	def __init__(self, produto, quantidade):
+		self.produto = produto
+		self.quantidade = quantidade
+
+class Carrinho():
+	def __init__(self):
+		self.itens = []
+		self.preco_total = 0
+
+	def adicionar_item(self, item):
+		self.itens.append(item)
+
+	def calcular_preco(self):
+		for i in self.itens:
+			self.preco_total += i.quantidade * i.produto.preco
+		return self.preco_total
+
+cafe = Produto(123, 3)
+maca = Produto(222, 5)
+item = Item(cafe, 2)
+item_maca = Item(maca, 9)
+carrinho = Carrinho()
+carrinho.adicionar_item(item)
+carrinho.adicionar_item(item_maca)
+print(carrinho.calcular_preco())
---
+++ veterinario/modelo.py
@@ -0,0 +1,27 @@
+class Cliente():
+	def __init__(self, cpf, nome, telefone):
+		self.cpf = cpf
+		self.nome = cpf
+		self.telefone = telefone
+
+class Animal():
+	def __init__(self, nome, especie, data_nascimento):
+		self.nome = nome
+		self.especie = especie
+		self.data_nascimento = data_nascimento
+
+class Produto():
+	def __init__(self, nome, preco, cod_produto):
+		self.nome = nome
+		self.preco = preco
+		self.cod_produto = cod_produto
+
+class Consulta():
+	def __init__():
+		def __init__(self, data, hora, finalidade, cliente, animal, produto):
+			self.data = data
+			self.hora = hora
+			self.finalidade = finalidade 
+			self.cliente = cliente
+			self.animal = animal
+			self.produto = produto
