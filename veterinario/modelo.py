08fb3fa1d7eeba783a6d319a00c2da9d5b623e6a
LuanaChiodini <lchiodini87@gmail.com>
2019-06-05 16:32:35 -0300

    testes na persistencia

--- veterinario/modelo.py
+++ veterinario/modelo.py
@@ -6,12 +6,13 @@ class Animal(peewee.Model):
 	nome_dono = peewee.CharField()
 	tipo_animal = peewee.CharField()
 	raca = peewee.CharField()
+	nome_animal = peewee.CharField()
 
 	class Meta():
 		database = db
 
 		def __str__(self):
-			return self.tipo_animal, self.raca, self.nome_dono
+			return self.tipo_animal, self.raca, self.nome_dono, self.nome_animal
 
 class Consulta(peewee.Model):
 	data = peewee.CharField()
@@ -29,7 +30,7 @@ class Consulta(peewee.Model):
 
 if __name__ == "__main__":
 	arq = "animal.db"
-	if os.path.exists(arq)
+	if os.path.exists(arq):
 		os.remove(arq)
 
 	try:
@@ -38,3 +39,15 @@ if __name__ == "__main__":
 
 	except peewee.OperationError as erro:
 		print("erro ao criar as tabelas: "+str(erro))
+
+	print("teste do animal")
+	a1 = Animal(nome_dono="Gustavo", tipo_animal="cachorro", raca="linguicinha", nome_animal="Sarchixa")
+	a1.save()
+	print(a1)
+	print("teste da consulta")
+	c1 = Consulta(data="2019-06-05", servico="Consulta", horario="14:00", animal=a1, confirma="sim", myID="3425576687sdf436")
+	
+
+	todos = Animal.select()
+	for i in todos:
+		print(i)
